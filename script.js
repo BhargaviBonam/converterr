dc5fef8bb1f743edf94220d2ad7f6b4b0b03224f
BhargaviBonam <114810911+BhargaviBonam@users.noreply.github.com>
2023-10-14 23:05:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
 
+//Define function to calculate based on button clicked.
+const calculate = (btnValue) => {
+  display.focus();
+  if (btnValue === "=" && output !== "") {
+    //If output has '%', replace with '/100' before evaluating.
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    //If DEL button is clicked, remove the last character from the output.
+    output = output.toString().slice(0, -1);
+  } else {
+    //If output is empty and button is specialChars then return
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
 
-let celsius = document.getElementById("celsius");
-let fahrenheit = document.getElementById("fahrenheit");
-
-function celToFar(){
-    let output = ( parseFloat(celsius.value) * 9/5 ) + 32;
-    fahrenheit.value = parseFloat(output.toFixed(2));
-}
-
-function farToCel(){
-    let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
-    celsius.value = parseFloat( output.toFixed(2));
-    console.log(output);
-}
-
-
+//Add event listener to buttons, call calculate() on click.
+buttons.forEach((button) => {
+  //Button click listener calls calculate() with dataset value as argument.
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
